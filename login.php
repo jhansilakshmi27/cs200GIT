6c2253a8811273c29f1903f277d1cdae9a070c34
jhansilakshmi27 <matchajh@iitbhilai.ac.in>
2023-10-12 23:34:39 +0530

    added first login file

---
+++ login.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>
+            Login
+        </title>
+    </head>
+    <body>
+        
+        <br>
+        
+        <h1><center><u>Login</u></center></h1>
+        <img src="https://img.freepik.com/free-vector/login-concept-illustration_114360-739.jpg?w=2000" width="300" alt="quote"  align="right" style="margin-right:10em;"/><br><br>
+        
+	<b>E-mail : </b><input  name="email"/><br><br>
+<b>Password : </b><input type="password" name="password"/><br><br>
+<input  type="submit" name="submit" value="submit"> <br><br><br>
+  
+        </body>
+        </html>
