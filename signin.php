9d22a8344cfe9408f6892e7636c98fa6cac6943d
gaurshivangi <gaur.shivangi320@gmail.com>
2023-10-13 00:23:39 +0530

    adding basic form on sign in page

---
+++ signin.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <title>
+            Sign-in
+        </title>
+    </head>
+    <body>
+
+
+<form method="POST" style="margin-left:5em;">
+EMAIL:<br>
+<input name="email" /><br><br>
+PASSWORD:<br>
+<input type="password" name="password" /><br><br>
+
+NAME:<br>
+<input name="name" /><br><br><BR>
+Since you're a new user we'd like a keep a track of your past medicals records.
+<h3>MEDICAL HISTORY.</h3><br>
+AGE:<br>
+<input type="number" name="age" /><br><br>
+<u>GENDER:</u><br>
+<input type="radio"  name="gender" value="male" /> Male<br>
+<input type="radio"  name="gender" value="female" />Female<br>
+<input type="radio"  name="gender" value="others" checked />Prefer not to say<br><br>
+
+<BR> <input  type="submit" name="submit" value="submit"> <br><br>
+
+
+
+	</body>
+	</html>
