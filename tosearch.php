6ca2e451eb53b3db35a17e43cd36ebd483693cc5
jhansilakshmi27 <matchajh@iitbhilai.ac.in>
2023-10-14 18:38:30 +0530

    html content written

---
+++ tosearch.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>
+            User Profile
+        </title>
+    </head>
+    <body>
+        <h1>HealthyMe</h1>
+        
+        <br>
+        <h1><center><u>USER INFORMATION</u></center></h1>
+        <img src="https://img.freepik.com/free-vector/user-research-concept-illustration_114360-8143.jpg?w=2000" width="400" alt="quote"  align="right" style="margin-right:5em;"/><br><br>
+        
+       
+        
+        </body>
+        </html>
