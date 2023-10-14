eb37d563476c0bf01f41965230141842cb96a113
jhansilakshmi27 <matchajh@iitbhilai.ac.in>
2023-10-14 18:48:17 +0530

    updated tosearch.php with php code

--- tosearch.php
+++ tosearch.php
@@ -11,7 +11,73 @@
         <h1><center><u>USER INFORMATION</u></center></h1>
         <img src="https://img.freepik.com/free-vector/user-research-concept-illustration_114360-8143.jpg?w=2000" width="400" alt="quote"  align="right" style="margin-right:5em;"/><br><br>
         
-       
+       <?php
+	#$fr = fopen("logininfo.txt", "r");
+	$matchemail = $_POST['email'];
+	$matchpassword = $_POST['password'];
+	$userinfo = file_get_contents("logininfo.txt");
+	$users = explode("\n", $userinfo);
+	$results = array();
+	$diseases = array();
+	foreach($users as $user)
+	{
+		$fields = explode(",", $user);
+		#echo "". $matchemail. "  " .$fields[0] . "<br>";
+		#echo "".$matchpassword  . "  " . $fields[1] . "<br>";
+		#echo "$fields[0] == $matchemail";
+		if ("$fields[0]" == "$matchemail" && "$fields[1]" == "$matchpassword")  
+			{
+				$results[] = array($fields[0], $fields[1]);
+				$user_name=$fields[2];
+				$gender=$fields[3];
+				$age=$fields[4];
+				for($i=5; $i < count($fields); $i++)
+				{
+					$diseases[] = $fields[$i];
+				}
+				break;
+			} 
+	}
+	
+	$queryinfo = file_get_contents("userinfo.txt");
+	$query = explode("\n", $queryinfo);
+	
+	
+	if (count($results) > 0)
+	{
+		
+		echo "<h3>Name: ".  $user_name . "<br><br></h3>";
+		echo "<h3>Gender: ".  $gender . "<br><br></h3>";
+		echo "<h3>Medical History: <br><br></h3>";
+		foreach ($diseases as $disease)
+		{
+    			echo "<li>". $disease . "</li><br>";
+		}
+		
+	}
+	else 
+	{
+		echo "<b>No results found.</b>";
+		header("Location: login.php");
+	}
+	
+	$queryinfo = file_get_contents("userinfo.txt");
+	$query = explode("\n", $queryinfo);
+	
+	foreach($query as $queries)
+	{
+		$field = explode(",", $queries);
+		
+		if($field[1] == $results[0][0])
+		{
+			echo "<h3><p>Past Queries: </p></h3>";
+			echo "<i> ". $field[2]."</i><br>";
+		}
+	}
+			
+			
+	
+?>
         
         </body>
         </html>
