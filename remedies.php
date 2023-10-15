52ab6091ea065d3ce883e0f51cf407f2144822b1
vaideeka <vaideekaa@iitbhilai.ac.in>
2023-10-15 11:35:51 +0530

    basic structure updated

---
+++ remedies.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>remedies</title>
+</head>
+<body>
+<div style="background-color:00674e; height:100; align:top">
+        
+        <h1 style="font-family:ways; text-align:center; color:white; font-size:70; ">HealthyMe</h1>
+</body>
+</html>
