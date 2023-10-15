a31faa22f09c3123ff16c66f0d52ebedf28c9424
vaideeka <vaideekaa@iitbhilai.ac.in>
2023-10-15 11:11:29 +0530

    basic structure updated

---
+++ diagnose.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>
+            diagnose
+        </title>
+    </head>
+    <body>
+        <div style="background-color:00674e; height:100; align:top">
+        
+        <h1 style="font-family:ways; text-align:center; color:white; font-size:70; ">HealthyMe</h1>
+        
+        </div>
+        <header>
+  	<h2>Diagnose</h2>
+	</header>
+        
+        
+        
+        
+        
+
+</body>
+</html>
