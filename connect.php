f6f1111d4c4844b6f1c87be0727a370380618267
gaurshivangi <gaur.shivangi320@gmail.com>
2023-10-15 10:08:14 +0530

    basic form format

---
+++ connect.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title>
+            connect
+        </title>
+    </head>
+    <body>
+        <div style="background-color:00674e; height:100; align:top">
+        
+        <h1 style="font-family:ways; text-align:center; color:white; font-size:70; ">HealthyMe</h1>
+        
+        </div>
+        
+        <h1 style="margin-left:1em;"> Feel free to provide a feedback or submit any queries. </h1>
+   
+        <form method="POST" style="margin-left:2em;">
+        <br height= '20' ><br>
+       
+        <b>
+NAME:<br>
+<input name="name" /><br><br>
+EMAIL:<br>
+<input name="email" /><br><br>
+QUERY/FEEDBACK: <br>
+<textarea rows="5" cols="40" name="address"></textarea><br><br>
+<input  type="submit" name="submit" value="submit"> <br><br>
+</b>
+</form>
+
+To view your past queries or responds click on <button onclick="document.location='login.php'">User Profile</button>
+<br><br> 
+
+        
+    </body>
+</html>
