3adb5163b2b6254e90799181ae70effbdd2bf615
vaideeka <vaideekaa@iitbhilai.ac.in>
2023-10-15 11:43:18 +0530

    table created

--- remedies.php
+++ remedies.php
@@ -6,5 +6,24 @@
 <div style="background-color:00674e; height:100; align:top">
         
         <h1 style="font-family:ways; text-align:center; color:white; font-size:70; ">HealthyMe</h1>
+</div>
+        <br><br>
+        <center>
+<table border="1" cellspacing="10" bgcolor=" #00674e " height="80%" width="83%">
+<tr bgcolor="#ffd13d"><th colspan="3"><h1><u>Remedies for <?php echo $_GET['disease']; ?>:</u></h1></th></tr>
+<tr bgcolor="#f1ffa2"><th><strong><h3>Home Remedies:</h3></strong></th><th><strong><h3>Dietary Advice:</h3></strong></th><th><strong><h3>OTC Medicines:</h3></strong></th></tr>
+
+</table>
+</center>
+<br>
+
+
+
+
+
+
+
+
+
 </body>
 </html>
