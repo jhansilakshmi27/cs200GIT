5a3d9b044614e830a1b79b7d587c776b9cfcd5f5
vaideeka <vaideekaa@iitbhilai.ac.in>
2023-10-15 12:39:45 +0530

    css code added

--- diagnose.php
+++ diagnose.php
@@ -3,6 +3,59 @@
         <title>
             diagnose
         </title>
+        
+       <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+header {
+  background-color:  #DAF7A6 ;
+  padding: 30px;
+  text-align: center;
+  font-size: 35px;
+  color: #00674e;
+}
+
+nav {
+  float: left;
+  width: 30%;
+  height: 760px; 
+  background:  #FFC300   ;
+  padding: 20px;
+}
+
+
+article {
+  float: left;
+  padding: 20px;
+  width: 70%;
+  background-color: white;
+  height: 760px; 
+}
+
+
+section::after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+footer {
+  background-color: 00674e ;
+  padding: 10px;
+  text-align: center;
+  color: white;
+}
+</style>
+        
     </head>
     <body>
         <div style="background-color:00674e; height:100; align:top">
